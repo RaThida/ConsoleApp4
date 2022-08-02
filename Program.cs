d8b5524e63b4b1bfc645a42a5e5f6ba75ff7f1f8
student <student@DG0072012>
2022-08-02 10:42:30 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class student
+    {
+        public string strName;
+        public string strCourse;
+        public string strGroup;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student obj1, obj2; // dealaring two object names of type student
+            obj1 = new student(); //instantatie obj1
+            obj2 = new student();
+            Console.Write("Enter the name for student 1: ");
+            obj1.strName = Console.ReadLine();
+            Console.Write("Enter the course for student 1: ");
+            obj1.strCourse = Console.ReadLine();
+            Console.Write("Enter the group name for student 1: ");
+            obj1.strGroup = Console.ReadLine();
+            Console.Write("Enter the")
+
+
+        }
+    }
+     
+    
+}
